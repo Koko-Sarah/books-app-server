c177dae7550bcce32879d9d7ee6e7839cbe54216
Kowser Kassa <kowserk7@gmail.com>
2017-12-19 14:46:16 -0800

    followed along in class

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const cors = require('cors');
@@ -6,24 +6,20 @@ const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
-const client = new pg.Client(process.env.DATABASE_URL);
 
+const client = new pg.Client(DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.get('/api/v1/books', (req, res) => {
+  client.query('SELECT * FROM books;')
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
 
-app.get('/books_app', (req, res) =>{
-  //call get all the books here
-  //using postgres Language SELECT * FROM books
-})
-
+app.all('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
-
-///////////////////** DATABASE LOADERS **////////////////
-// I don't think we need these console log req adn res.body in both serer and client
