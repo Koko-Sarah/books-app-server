47442f1b7e4f8f1b201fc365bedf50922f70dc81
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-19 18:25:10 -0800

    database get route established in server.js

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-'use strict'
-
+'use strict';
+//Application Dependencies
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 
+// Application Setup
 const app = express();
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
-const client = new pg.Client(process.env.DATABASE_URL);
 
+//DATABASE Setup
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.get('/api/v1/books', (req, res) => {
+  client.query('SELECT book_id, title, author, image_url FROM books;')
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
 
-app.get('/books_app', (req, res) =>{
-  //call get all the books here
-  //using postgres Language SELECT * FROM books
-})
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.all('*', (req, res) => res.redirect(CLIENT_URL));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
-///////////////////** DATABASE LOADERS **////////////////
-// I don't think we need these console log req adn res.body in both serer and client
