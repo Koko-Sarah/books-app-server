39732a7d2775385ef0e49857083ee2aed1b23d6d
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-19 16:41:59 -0800

    added notes and code snippets from class

--- server.js
+++ server.js
@@ -7,22 +7,31 @@ const pg = require('pg');
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
+//Data base set up
 const client = new pg.Client(process.env.DATABASE_URL);
-
 client.connect();
 client.on('error', err => console.error(err));
+//these console logs will run in the REPL where we are running our server, so in gitBASH
 
+//what's cors? definition of middleware:
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+//app.get('*', (req, res) => res.redirect(CLIENT_URL));//this is a "wildcard" route, if you do anything weird, I'll kick you back to the client
+//scott uses:
+app.all('*', (req, res) => res.redirect(CLIENT_URL));
 
-// app.get('/api/v1/books', (req, res) =>{
+//now we set up a route to get stuff from our database, our server is "an API" it does not serve up html, just gets request and sends responses, this is defining the address that the database info will be served to, if you know use this end point the wildcard is triggered
+//when we do this usually the api is called out /api, the version number and / the name
 
-//   //call get all the books here
-//   //using postgres Language SELECT * FROM books
-// });
+app.get('/api/v1/books', (req, res) =>{// note here we use res
 
+  client.query('SELECT book_id, title, author, image_url, isbn FROM books;')//find more info on these queries under postgres documentation
+    .then(results => res.send(results.rows))//here we must use results to distinuish from res
+    .catch(console.error);
+});
+
+//here we are setting up our port, our little listener
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 
