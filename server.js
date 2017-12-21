08322302955d4e250a4f6c35feff59ba1a520166
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-20 16:12:59 -0800

    added some functionality, mostly working

--- server.js
+++ server.js
@@ -21,21 +21,24 @@ client.on('error', err => console.error(err));
 app.use(cors());
 
 app.get('/api/v1/books', (req, res) => {
+  console.log('successaaaasda');
   client.query('SELECT * FROM books;')
 
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
 
-//route for book detail by id
-app.get('/api/v1/books/1', (req,res)=> {
+//route for book detail by id changing route to :id, this :id is like a variable that we can plug in from document, this is express specific syntax, also common to outher routers, :parameter is what :id is doing, these can take regex patterns too, you can do :parameter? making it optional
+
+app.get('/api/v1/books/:id', (req,res)=> {
   client.query(`SELECT * FROM books 
-  WHERE book_id=5; `)
+  WHERE book_id=$1;`, [req.params.id])
+    .then(console.log(res))
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
 
 
-app.all('*', (req, res) => res.redirect(CLIENT_URL));
+// app.all('*', (req, res) => res.redirect(CLIENT_URL));//the asterix is a regex pattern match
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
